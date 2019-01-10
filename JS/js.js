662d16c3c9bc3464876e84e582dfdfb54b4dc2a4
JuanPaolo2 <46500189+JuanPaolo2@users.noreply.github.com>
2019-01-10 12:16:51 +0000

    5

---
+++ JS/js.js
@@ -0,0 +1,32 @@
+var slideindex =1;
+slides(slideIndex);
+
+function plusSlides(n) {
+    slides(slideIndex += n);
+  }
+
+function currentSlide(n) {
+    slides(slideIndex = n);
+  }
+//slide functions for images slider in the home page
+function slides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block"; 
+    dots[slideIndex-1].className += " active";
+}
+
+
+
+
+
+
