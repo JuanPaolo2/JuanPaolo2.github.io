3de849a895fb4e130698de5a66d00ade1b852cd5
JuanPaolo2 <46500189+JuanPaolo2@users.noreply.github.com>
2022-10-08 20:11:39 +0100

    Update js2.js

--- JS/js2.js
+++ JS/js2.js
@@ -10,7 +10,7 @@ for(let i = 0; i < fill.length; i++) {
         draggedItem = item;
         setTimeout(function () {
             item.style.display = 'none';
-        },0)
+        },0);
         
     });
     
@@ -19,7 +19,7 @@ for(let i = 0; i < fill.length; i++) {
             draggedItem.style.display = 'block';
             draggedItem = null;
         },0);
-    })
+    });
     
     
     for(let j = 0; j < empties.length; j++){
